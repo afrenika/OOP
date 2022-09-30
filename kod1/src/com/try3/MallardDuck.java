980d2ed7d3369607c24eadc03617cd2ad7923385
afrenika <114728654+afrenika@users.noreply.github.com>
2022-09-30 11:26:29 +0300

    Create MallardDuck.java

---
+++ kod1/src/com/try3/MallardDuck.java
@@ -0,0 +1,18 @@
+package com.try3;
+
+
+
+public class MallardDuck extends Duck {
+
+    public MallardDuck() {
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+
+    @Override
+    void display() {
+        System.out.println("I'm MallardDuck.");
+    }
+
+
+}
